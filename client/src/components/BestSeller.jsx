2321903da10548b6a3697af1b23464fbad6cda70
yashmika <yashmikashashimal07603@gmail.com>
2025-05-08 13:03:04 +0530

    bottom banner

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -10,8 +10,11 @@ const BestSeller = () => {
     <div>
       <div className='mt-16'>
         <p className='text-2xl font-medium md:text-3xl'>Best Sellers</p>
-        <div>
-          <ProductCard product={products[0]}/>
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6'>
+        {products.filter((product)=> product.inStock).slice(0,5).map((product, index)=>(
+          <ProductCard key={index} product={product}/>
+        ))}
+          
         </div>
       </div>
     </div>
