796275f6d76b7c8e133c55455dae3337733fd87a
yashmika <yashmikashashimal07603@gmail.com>
2025-05-11 10:50:16 +0530

    clasname

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -39,7 +39,7 @@ const MyOrders = () => {
                   <p >Category: {item.product.category}</p>
                 </div>
               </div>
-              <div className='text-lg text-primary font-medium'>
+              <div className='flex flex-col justify-center md:ml-8 mb-4 md:mb-0'>
                 <p>Quantity: {item.quantity || "1"}</p>
                 <p>States: {item.states}</p>
                 <p>Date: {new Date(order.createdAt).toLocaleDateString()}</p>
